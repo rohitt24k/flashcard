a079209c4dfc488e43895847f0a43efa8d852f87
rohitt24k <rohitt24k@gmail.com>
2024-08-12 22:44:22 +0530

    backend deployment changes

--- frontend/src/lib/axiosInstance.js
+++ frontend/src/lib/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://flashcard-henna-one.vercel.app/api",
   withCredentials: true,
 });
 
